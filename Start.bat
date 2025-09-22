a47beead43081308481c558b59a741682a4bb305
cccp999 <144536782+cccp999@users.noreply.github.com>
2025-09-23 01:13:45 +0800

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd %~dp0
 set NODE_ENV=production
-call npm install --no-audit --no-fund --loglevel=error --no-progress --omit=dev
+call npm install --no-audit --no-fund --quiet --omit=dev  --registry=https://registry.npmmirror.com/
 node server.js %*
 pause
 popd
